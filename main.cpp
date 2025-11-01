c23890b240ae0a73b3532988ec736ae56a6ae53b
vamaria06-ux <mashaulanova545@gmail.com>
2025-11-01 14:00:17 +0300

    matrix input ouput

--- main.cpp
+++ main.cpp
@@ -38,6 +38,29 @@ int ** construct(int ** mtx, int init , size_t rows, size_t cols)
   }
   return mtx;
 }
+void input( int ** mtx, size_t rows, size_t cols)
+{
+  for (size_t i=0; i < rows;i++)
+  {
+    for(size_t j=0; j < cols, j++)
+   {
+      std::cin>>mtx[i][j];
+   }
+  }
+}
+
+void output( int ** mtx, size_t rows, size_t cols)
+{
+  for (size_t i=0; i < rows; ++i)
+  {
+    //std::cout<<mtx[i][0];
+    for(size_t j=0; j < cols, ++j)
+   {
+      std::cout<<'' <<mtx[i][j];
+   }
+    std::cout << '\n';
+  }
+}
 
 int main(){
   size_t r=0,c=0;
@@ -59,6 +82,14 @@ int main(){
   }
   std::cout<<"created\n";
   construct(matrix,2,r,c);
-  std::cout<<matrix[0][0]<<"\n";
+  //std::cout<<matrix[0][0]<<"\n";
+  input(matrix,r,c);
+  if(!std::cin)
+  {
+    destroy(matrix,r);
+    std::cerr<<"Input error\n";
+    return 1;
+  }
+  output(matrix,r,c);
   destroy(matrix,r);
 }
