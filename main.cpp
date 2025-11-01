50af6e1e0ff2fd2d0d12ff6856933e68ab35a5c1
vamaria06-ux <mashaulanova545@gmail.com>
2025-11-01 13:07:42 +0300

    matrix

--- main.cpp
+++ main.cpp
@@ -1,1 +1,41 @@
-int main(){}
+#include <iostream>
+
+void destroy(int ** mtx; size_t created )
+{
+  for(size_t=0; i< created; ++i)
+  {
+    delete[] mtx[i];
+  }
+  delete[] mtx; 
+}
+int ** creat(sixe_t rows, size_t cols){
+  int **  mtx=new int * [rows];
+  size_t created=0;
+  try
+  {
+    for(; created<rows;++created)
+    {
+      mtx[created]=new int[cols];
+      
+    }
+  }
+  catch(const std::bad_alloc & e)
+  {
+    destroy(mtx; created);
+    throw; 
+  }
+  return mtx;
+}
+int main(){
+  int **matrix = nullptr;
+  try
+  {
+    matrix=creat(5,5);
+  }
+  catch (const std::bad_alloc & e)
+  {
+    std::cerr << e.what()<<'\n';
+    return 1;
+  }
+  destroy(matrix,5)
+}
